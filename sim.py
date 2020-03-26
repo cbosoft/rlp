c99442537504ce9f430bae29865cfdffc39448d1
Chris Boyle <christopher.boyle.101@strath.ac.uk>
2020-03-26 22:09:10 +0000

    Only recalculate triangles as necessary

--- sim.py
+++ sim.py
@@ -10,84 +10,65 @@ class Sim:
     def __init__(self, L):
         self.particles = list()
         self.L = L
+        self.triangles = list()
 
 
     def generate_particle(self, diameter=1.0):
 
-        position = np.multiply(self.L, np.random.random((3)))
-        # i = 0
-        # while any([self.get_distance2(position, particle) < (diameter+particle.diameter)/2.0 for particle in self.particles]):
-        #     position = np.multiply(self.L, np.random.random((3)))
-        #     i += 1
-        #     if i > 1000:
-        #         raise Exception("took too long to generate")
+        print(f'generating particle {len(self.particles)} ', end='')
 
+        position = np.multiply(self.L, np.random.random((3)))
         position[2] = self.L
         new_particle = Particle(position, diameter)
         self.settle(new_particle)
 
-        if new_particle.settled:
-            self.particles.append(new_particle)
+        if not new_particle.settled:
+            raise Exception('settling failed')
+
+        self.update_triangles()
+        self.particles.append(new_particle)
 
 
-    def find_triangles(self, thresh):
-        # TODO don't re-form tringle list every time, just add new particle's trianlges to the current.
-        triplets = itt.product(self.particles, self.particles, self.particles)
-        triangles = list()
-        trangles_ids = list()
-        for pi, pj, pk in triplets:
+    def update_triangles(self):
+        if not self.particles:
+            return
+        pairs = itt.product(self.particles[:-1], self.particles[:-1])
+        pi = self.particles[-1]
+        thresh = pi.diameter
+        triangles_ids = list()
+        for pj, pk in pairs:
             ids = set(sorted([pi.id, pj.id, pk.id]))
             
             if len(ids) < 3:
                 continue
 
-            if ids in trangles_ids:
+            if ids in triangles_ids:
                 continue
+            triangles_ids.append(ids)
 
             t = Triangle(pi.position, pj.position, pk.position)
             c = t.centre()
 
-            # if any([self.get_distance(c, t.a) < thresh,
-            #     self.get_distance(c, t.b) < thresh,
-            #     self.get_distance(c, t.c) < thresh]):
-            #     print('edges too small')
-            #     continue
-
             if any([e > 2.0*thresh for e in t.edge_lengths()]):
                 continue
 
-            triangles.append(t)
-        return triangles
+            self.triangles.append(t)
 
 
     def settle(self, particle):
         intersections = list()
-        for triangle in self.find_triangles(particle.diameter):
+        for triangle in self.triangles:
             if triangle.intersects(particle.position, D=2):
                 intersections.append(triangle)
 
-        print("settling particle", end='')
+        print('setting', end='')
         if not intersections:
+            # TODO not falling into a triangle means the particle will not be supported, but doesn't mean it won't fall.
+            # check for nearest neighbours, if they have an XY-separation less than radius, then will tumble away
             particle.settle_to(position_z=0.0)
-            print(" on floor")
+            print(' on floor')
         else:
             # TODO select proper height
             particle.settle_to(position_z=1.0)
-            print(" on top")
-
-
-
-    def get_distance(self, p1, p2):
-        if isinstance(p1, Particle):
-            p1 = p1.position
-        if isinstance(p2, Particle):
-            p2 = p2.position
-        return get_distance(p1, p2)
-
-
-    def get_distance2(self, p1, p2):
-        if isinstance(p1, Particle):
-            p1 = p1.position
-        if isinstance(p2, Particle):
-            p2 = p2.position
-        return get_distance(p1[:-1], p2[:-1])
+            # TODO if particle settles in triangle, triangle is blocked - remove from list
+            print(' on top')
