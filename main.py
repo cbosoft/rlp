1fe335c2a07bf3f283d954c89baf7a89bd396f44
Chris Boyle <christopher.boyle.101@strath.ac.uk>
2020-03-26 22:09:23 +0000

    Added arg parsing and csv output to main

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import sys
+
 import numpy as np
 from matplotlib import pyplot as plt
 
@@ -5,34 +7,61 @@ from particle import Particle
 from sim import Sim
 
 if __name__ == "__main__":
-    sim = Sim(10.0)
-    for i in range(50):
-        sim.generate_particle()
-    # TODO test triangle intersection and triangle get
 
-    triangles = sim.find_triangles(2)
+    argv = sys.argv[1:]
+    args = {
+            '--output':'out.csv',
+            '--number':100,
+            '--length':10.0,
+            '--diameter':1.0,
+            '--plot':None
+        }
+    if argv:
+        assert len(argv) % 2 == 0
+        specified_args = {k:v for k,v in zip(argv, argv[1:])}
+        args = {**args, **specified_args}
 
-    ax = plt.axes([0, 0, 1, 1], projection='3d')
+    print(sys.argv)
 
-    u, v = np.mgrid[0:2*np.pi:20j, 0:np.pi:10j]
-    X = np.cos(u)*np.sin(v)*0.5
-    Y = np.sin(u)*np.sin(v)*0.5
-    Z = np.cos(v)*0.5
 
-    for particle in sim.particles:
-        #plt.gca().add_patch(plt.Circle(particle.position[:2], 0.5))
+    print('generating particles')
+    sim = Sim(float(args['--length']))
+    for i in range(int(args['--number'])):
+        sim.generate_particle(float(args['--diameter']))
 
-        #x, y, z = np.add(particle.position, [X, Y, Z] )
-        x = np.add(X, particle.position[0])
-        y = np.add(Y, particle.position[1])
-        z = np.add(Z, particle.position[2])
-        ax.plot_surface(x, y, z, color="r")
+    print('outputting')
+    with open(args['--output'], 'w') as f:
+        for particle in sim.particles:
+            x, y, z = particle.position
+            f.write(f'{x},{y},{z}\n')
 
-    for triangle in triangles:
-        x, y, z = zip(*triangle.vertices())
-        ax.plot(x, y, z, '--', color='0.5')
-    ax.set_xlim([0,10])
-    ax.set_ylim([0,10])
-    ax.set_zlim([0,10])
-    plt.show()
+
+    if args['--plot']:
+        print('plotting')
+
+        triangles = sim.triangles
+
+        ax = plt.axes([0, 0, 1, 1], projection='3d')
+
+        u, v = np.mgrid[0:2*np.pi:20j, 0:np.pi:10j]
+        X = np.cos(u)*np.sin(v)*0.5
+        Y = np.sin(u)*np.sin(v)*0.5
+        Z = np.cos(v)*0.5
+
+        for particle in sim.particles:
+            x = np.add(X, particle.position[0])
+            y = np.add(Y, particle.position[1])
+            z = np.add(Z, particle.position[2])
+            ax.plot_surface(x, y, z, color="r")
+
+            # x, y, z = particle.position
+            # ax.plot([x], [y], [z], 'C0o')
+
+        for triangle in triangles:
+            x, y, z = zip(*triangle.vertices())
+            ax.plot(x, y, z, '--', color='0.5')
+        ax.set_xlim([0,sim.L])
+        ax.set_ylim([0,sim.L])
+        ax.set_zlim([0,sim.L])
+        plt.savefig(args['--plot'])
 
