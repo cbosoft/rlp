aad4af95ac70985f0d0e05949f6d18bd44ffafae
Chris Boyle <christopher.boyle.101@strath.ac.uk>
2020-04-01 18:39:22 +0100

    Revert vec mag2 method after fixing dot product

--- src/vec.hpp
+++ src/vec.hpp
@@ -109,11 +109,7 @@ class VecN {
 
     double magnitude2() const noexcept
     {
-      double tot = 0.0;
-      for (int i = 0; i < N; i++) {
-        tot += this->v[i] * this->v[i];
-      }
-      return tot;
+      return this->dot(*this);
     }
 
     double magnitude() const noexcept
