0c46d62cacc59c42c0c3d0100a565b8a2f36b2d3
Chris Boyle <christopher.boyle.101@strath.ac.uk>
2020-04-01 17:38:05 +0100

    Fixed bug in calculating magnitude

--- src/vec.hpp
+++ src/vec.hpp
@@ -105,7 +105,11 @@ class VecN {
 
     double magnitude2() const noexcept
     {
-      return this->dot(*this);
+      double tot = 0.0;
+      for (int i = 0; i < N; i++) {
+        tot += this->v[i] * this->v[i];
+      }
+      return tot;
     }
 
     double magnitude() const noexcept
