d0da90c276834e9ea1c33b6520b60579564cf18a
Chris Boyle <christopher.boyle.101@strath.ac.uk>
2020-04-01 18:19:52 +0100

    Added further vector functionality tests

--- src/test/main.cpp
+++ src/test/main.cpp
@@ -5,5 +5,9 @@ int main(void)
 {
   int counter = 0;
   VectorMagnitudeTest(counter).run_each();
+  VectorCrossTest(counter).run_each();
+  VectorOrthoComponentTest(counter).run_each();
+
+  // vertex tests
   VertexInteractionTest(counter).run_each();
 }
--- src/test/vec.hpp
+++ src/test/vec.hpp
@@ -4,17 +4,17 @@
 #include "../vec.hpp"
 #include "../epsilon.hpp"
 
-class VectorMagnitudeTest : public virtual TestRunner<double> {
+class VectorMagnitudeTest : public virtual TestRunner<Vec3, double> {
 
   public:
     VectorMagnitudeTest(int &counter) 
       : TestRunner(counter, "Vector test (magnitude)")
     {
-      this->points = {
-        {1.0, 0.0, 0.0},
-        {0.0, 1.0, 0.0},
-        {0.0, 0.0, 1.0},
-        {3.0, 4.0, 12.0}
+      this->input_data = {
+        Vec3({1.0, 0.0, 0.0}),
+        Vec3({0.0, 1.0, 0.0}),
+        Vec3({0.0, 0.0, 1.0}),
+        Vec3({3.0, 4.0, 12.0})
       };
       this->expected_results = {
         1.0,
@@ -24,18 +24,87 @@ class VectorMagnitudeTest : public virtual TestRunner<double> {
       };
     }
 
-    void run(int test_index) override
+    void run(Vec3 vector, double expected_result) override
     {
-      Vec3 v(this->points[test_index]);
-
-      double result = v.magnitude();
-      double expected_result = this->expected_results[test_index];
+      double result = vector.magnitude();
 
       if (std::fabs(result-expected_result) < EPSILON) {
         this->pass();
       }
       else {
-        this->fail(Formatter() << "Magnitude of " << v.repr() << " calculated as " << result << " instead of " << expected_result << "." );
+        this->fail(Formatter() << "Magnitude of " << vector.repr() << " calculated as " << result << " instead of " << expected_result << "." );
+      }
+    }
+};
+
+
+class VectorCrossTest : public virtual TestRunner<std::pair<Vec3, Vec3>, Vec3> {
+
+  public:
+    VectorCrossTest(int &counter) 
+      : TestRunner(counter, "Vector test (cross multiplication)")
+    {
+      this->input_data = {
+        std::make_pair(Vec3({1.0, 0.0, 0.0}), Vec3({0.0, 0.0, 0.0})),
+        std::make_pair(Vec3({1.0, 0.0, 0.0}), Vec3({1.0, 0.0, 0.0})),
+        std::make_pair(Vec3({1.0, 0.0, 0.0}), Vec3({0.0, 1.0, 0.0}))
+      };
+      this->expected_results = {
+        Vec3({0.0, 0.0, 0.0}),
+        Vec3({0.0, 0.0, 0.0}),
+        Vec3({0.0, 0.0, 1.0})
+      };
+    }
+
+    void run(std::pair<Vec3, Vec3> pair, Vec3 expected_result) override
+    {
+      Vec3 left_vector = pair.first, right_vector = pair.second;
+
+      Vec3 result = left_vector.cross(right_vector);
+
+      if (result == expected_result) {
+        this->pass();
+      }
+      else {
+        this->fail(Formatter() 
+            << "Magnitude of " 
+            << left_vector << " ✕ " << right_vector
+            << " calculated as " << result
+            << " instead of " << expected_result << "." );
+      }
+    }
+};
+
+
+class VectorOrthoComponentTest : public virtual TestRunner<std::pair<Vec3, Vec3>, Vec3> {
+
+  public:
+    VectorOrthoComponentTest(int &counter) 
+      : TestRunner(counter, "Vector test (orthogonal component)")
+    {
+      this->input_data = {
+        std::make_pair(Vec3({1.0, 0.0, 0.0}), Vec3({0.0, 1.0, 0.0}))
+      };
+      this->expected_results = {
+        Vec3({0.0, 0.0, 0.0})
+      };
+    }
+
+    void run(std::pair<Vec3, Vec3> pair, Vec3 expected_result) override
+    {
+      Vec3 left_vector = pair.first, right_vector = pair.second;
+
+      Vec3 result = left_vector.component_along(right_vector);
+
+      if (result == expected_result) {
+        this->pass();
+      }
+      else {
+        this->fail(Formatter()
+            << "Component of " << left_vector
+            << " along " << right_vector
+            << " calculated as " << result
+            << " instead of " << expected_result << "." );
       }
     }
 };
