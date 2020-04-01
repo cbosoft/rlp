ef5b943b83a5c652b7039ded5f4aea5d7f4794f7
Chris Boyle <christopher.boyle.101@strath.ac.uk>
2020-04-01 14:40:29 +0100

    Use templates for box effective positions and separations

--- src/box.hpp
+++ src/box.hpp
@@ -25,8 +25,14 @@ class PeriodicBox {
     PeriodicBox(double L);
     ~PeriodicBox();
 
+    template<int N>
+    VecN<N> get_effective_separation(const VecN<N> &a, const VecN<N> &b) const noexcept;
     Vec3 get_effective_separation(const Vec3 &a, const Vec3 &b) const noexcept;
+    Vec2 get_effective_separation(const Vec2 &a, const Vec2 &b) const noexcept;
+    template<int N>
+    VecN<N> get_effective_position(const VecN<N> &a) const noexcept;
     Vec3 get_effective_position(const Vec3 &a) const noexcept;
+    Vec2 get_effective_position(const Vec2 &a) const noexcept;
 
     void set_L(double L);
     double get_L();
--- src/box_bounds.cpp
+++ src/box_bounds.cpp
@@ -1,15 +1,22 @@
 #include "box.hpp"
 
-Vec3 PeriodicBox::get_effective_separation(const Vec3 &a, const Vec3 &b) const noexcept
+template<int N>
+VecN<N> PeriodicBox::get_effective_separation(const VecN<N> &a, const VecN<N> &b) const noexcept
 {
-  Vec3 dr = a - b;
+  VecN<N> dr = a - b;
   dr = dr - (dr*this->invL).nearbyint()*this->L;
   return dr;
 }
 
-Vec3 PeriodicBox::get_effective_position(const Vec3 &a) const noexcept
+template<int N>
+VecN<N> PeriodicBox::get_effective_position(const VecN<N> &a) const noexcept
 {
-  Vec3 rv = a;
+  VecN<N> rv = a;
   rv -= (rv * this->invL).floor() * this->L;
   return rv;
 }
+
+Vec3 PeriodicBox::get_effective_separation(const Vec3 &a, const Vec3 &b) const noexcept { return this->get_effective_separation<>(a, b); }
+Vec2 PeriodicBox::get_effective_separation(const Vec2 &a, const Vec2 &b) const noexcept { return this->get_effective_separation<>(a, b); }
+Vec3 PeriodicBox::get_effective_position(const Vec3 &a) const noexcept { return this->get_effective_position<>(a); }
+Vec2 PeriodicBox::get_effective_position(const Vec2 &a) const noexcept { return this->get_effective_position<>(a); }
