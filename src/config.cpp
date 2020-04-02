db5de92d4b92b161dc8131e66708e47f80646248
Chris Boyle <christopher.boyle.101@strath.ac.uk>
2020-04-02 16:05:39 +0100

    Ensure mode is truncate for log file

--- src/config.cpp
+++ src/config.cpp
@@ -11,7 +11,7 @@ ConfigGenerator::ConfigGenerator(double L)
 void ConfigGenerator::output_configuration(std::string file_path)
 {
   const std::vector<Particle *> particles = this->box.get_particles();
-  std::ofstream of(file_path);
+  std::ofstream of(file_path, std::ios::trunc);
 
   for (const Particle *particle : particles) {
     of << particle->get_diameter() << ", " << particle->get_position().to_csv() << std::endl;
