5fe0fd8519baa2b9b004be491f6646d881ca1189
Chris Boyle <christopher.boyle.101@strath.ac.uk>
2020-04-02 15:42:09 +0100

    Added timer code to configgen main

--- src/config_generate.cpp
+++ src/config_generate.cpp
@@ -1,12 +1,21 @@
+#include <chrono>
+
 #include "random.hpp"
 #include "vec.hpp"
 #include "config.hpp"
 
+#define CLOCK std::chrono::steady_clock
+
 void ConfigGenerator::generate_particles(int n)
 {
+
+  double generation_duration = -1;
   for (int i = 0; i < n; i++) {
-    std::cerr << i << std::endl;
+    std::cerr << "(" << generation_duration << ") " << i << " ";
+    auto before = CLOCK::now();
     this->generate_particle();
+    auto after = CLOCK::now();
+    generation_duration = static_cast<double>((after - before).count()) * CLOCK::duration::period::num / CLOCK::duration::period::den;
   }
 }
 
