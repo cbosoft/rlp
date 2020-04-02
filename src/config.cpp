8337c4032e97b23442fec433a9224d8352e31226
Chris Boyle <christopher.boyle.101@strath.ac.uk>
2020-04-02 16:06:04 +0100

    Output box size too

--- src/config.cpp
+++ src/config.cpp
@@ -13,6 +13,7 @@ void ConfigGenerator::output_configuration(std::string file_path)
   const std::vector<Particle *> particles = this->box.get_particles();
   std::ofstream of(file_path, std::ios::trunc);
 
+  of << this->box.get_L() << std::endl;
   for (const Particle *particle : particles) {
     of << particle->get_diameter() << ", " << particle->get_position().to_csv() << std::endl;
   }
