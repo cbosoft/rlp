e0b73f92dfa3c8ee16935158cd799c6232c80fe1
Chris Boyle <christopher.boyle.101@strath.ac.uk>
2020-04-02 13:22:50 +0100

    Added arg parsing

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,57 @@
 #include "config.hpp"
 #include "test/run_tests.hpp"
+#include "exception.hpp"
 
-int main(void)
+#define EITHER(A,B) ((strcmp(argv[i], A) == 0) || (strcmp(argv[i], B) == 0))
+
+int main(int argc, const char **argv)
 {
 
-  // TODO argument parsing
+  struct {
+    int number;
+    double length;
+    const char *output_path;
+    // double mean;
+    // double std;
+    bool run_tests;
+  } args = {
+    .number = 100,
+    .length = 10.0,
+    .output_path = "out.csv",
+    // .mean = 1.0,
+    // .std = 0.0,
+    .run_tests = true
+  };
 
-  run_tests();
+  argc--; argv++;
+  for (int i = 0; i < argc; i++) {
+    if (EITHER("-n", "--number")) {
+      args.number = std::atoi(argv[++i]);
+    }
+    else if (EITHER("-l", "--length")) {
+      args.length = std::atof(argv[++i]);
+    }
+    else if (EITHER("-o", "--output-path")) {
+      args.output_path = argv[i];
+    }
+    // else if (EITHER("-m", "--mean")) {
+    //   args.mean = atof(argv[++i]);
+    // }
+    // else if (EITHER("-s", "--std")) {
+    //   args.std = atof(argv[++i]);
+    // }
+    else if (strcmp("--dont-run-tests", argv[i]) == 0) {
+      args.run_tests = false;
+    }
+    else {
+      throw ArgumentError(Formatter() << "Unrecognised argument \"" << argv[i] << "\".");
+    }
+  }
 
-  ConfigGenerator cg = ConfigGenerator(10.0);
-  cg.generate_particles(1000);
+  if (args.run_tests)
+    run_tests();
+
+  ConfigGenerator cg = ConfigGenerator(args.length);
+  cg.generate_particles(args.number);
+  cg.output_configuration(args.output_path);
 }
