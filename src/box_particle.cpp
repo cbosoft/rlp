0716be2a64e8c296ba20a75d780608166b233d6a
Chris Boyle <christopher.boyle.101@strath.ac.uk>
2020-04-02 09:36:32 +0100

    Added status indication in particle settling method

--- src/box_particle.cpp
+++ src/box_particle.cpp
@@ -23,6 +23,8 @@ void PeriodicBox::add_particle(Particle *p)
 void PeriodicBox::settle_particle(Particle *p, int n, int recursion_limit)
 {
 
+  std::cerr << this->arrangements.size() << " " << p->get_position() << "-->";
+
   if (n >= recursion_limit)
     throw RecursionError("Exceeded recursion depth!");
 
@@ -35,18 +37,24 @@ void PeriodicBox::settle_particle(Particle *p, int n, int recursion_limit)
   if (interacting_arrangements.size() == 0) {
     p->set_z(0.0);
     p->set_settled();
+    std::cerr << p->get_position() << "(FLOOR)!" << std::endl;
     return;
   }
 
   ArrangementComparator comparator(p);
   interacting_arrangements.sort(comparator);
+
   ParticleArrangement *interacting_arrangement = interacting_arrangements.front();
+
+  std::cerr << interacting_arrangement->get_type();
 
   Vec3 new_position = interacting_arrangement->get_interaction_result(p);
   new_position = this->get_effective_position(new_position);
   p->set_position(new_position);
 
   if (interacting_arrangement->is_final()) {
+    // TODO remove this arrangement from main list of
+    std::cerr << p->get_position() << "!" << std::endl;
     p->set_settled();
   }
   else {
