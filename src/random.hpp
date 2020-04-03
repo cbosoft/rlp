b0b8c79dcfb3c0a28b043b5f69fa9e036c9f2ada
Chris Boyle <christopher.boyle.101@strath.ac.uk>
2020-04-03 23:36:20 +0100

    Added fucns to choose random item from iterable

--- src/random.hpp
+++ src/random.hpp
@@ -1,7 +1,24 @@
 #pragma once
+
+#include <iterator>
 #include <random>
 
 void seed(int s);
 void seed(std::seed_seq s);
 double urand(double min, double max);
 double nrand(double mean, double std);
+
+// https://stackoverflow.com/a/16421677/10042036
+template<typename Iter, typename RandomGenerator>
+Iter select_randomly(Iter start, Iter end, RandomGenerator& g) {
+    std::uniform_int_distribution<> dis(0, std::distance(start, end) - 1);
+    std::advance(start, dis(g));
+    return start;
+}
+
+template<typename Iter>
+Iter select_randomly(Iter start, Iter end) {
+    static std::random_device rd;
+    static std::mt19937 gen(rd());
+    return select_randomly(start, end, gen);
+}
