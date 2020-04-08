2198713f2b435f7132f125149d701b307959cd61
Chris Boyle <christopher.boyle.101@strath.ac.uk>
2020-04-08 14:08:13 +0100

    Add volume fraction to config output

--- src/config.cpp
+++ src/config.cpp
@@ -15,9 +15,10 @@ void ConfigGenerator::output_configuration(std::string file_path)
   const std::vector<Particle *> particles = this->box.get_particles();
   std::ofstream of(file_path, std::ios::trunc);
 
-  of << this->box.get_L() << std::endl;
+  of << this->box.get_L() << ", " << this->box.get_volume_fraction() << std::endl;
   for (const Particle *particle : particles) {
     of << particle->get_diameter() << ", " << particle->get_position().to_csv() << std::endl;
   }
+
 
 }
