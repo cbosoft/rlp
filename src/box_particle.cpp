4b617570320aa57e979a1a346ab4e42c0006849a
Chris Boyle <christopher.boyle.101@strath.ac.uk>
2020-04-08 16:03:37 +0100

    Use average height as cut off, mesh method seems buggy

--- src/box_particle.cpp
+++ src/box_particle.cpp
@@ -119,39 +119,47 @@ void PeriodicBox::settle_particle(Particle *p, int n, int recursion_limit)
 
 double PeriodicBox::get_lowest_surface_height(int n) const
 {
-
-  double dL = this->L / double(n);
-  double **mesh = new double*[n];
-  for (int i = 0; i < n; i++)
-    mesh[i] = new double[n];
-
+  (void)n;
+  double tot = 0.0;
   for (auto particle : this->particles) {
-    Vec3 v = particle->get_position();
-    double px = v.X();
-    double py = v.Y();
-    double pz = v.Z();
-
-    int ix = int(px/dL);
-    int iy = int(py/dL);
-
-    if ((ix > (n-1)) or (iy > (n-1)))
-      throw MathError("Index error in get_lowest_surface_height");
-
-    if (pz > mesh[ix][iy])
-      mesh[ix][iy] = pz;
+    tot += particle->get_position().Z();
   }
+  double v = tot/double(this->particles.size());
+  std::cerr << v << std::endl;
+  return v;
 
-  double lowest = mesh[0][0];
-  for (int i = 0; i < n; i++) {
-    for (int j = 0; j < n; j++) {
-      if (mesh[i][j] < lowest)
-        lowest = mesh[i][j];
-    }
-  }
+  // double dL = this->L / double(n);
+  // double **mesh = new double*[n];
+  // for (int i = 0; i < n; i++)
+  //   mesh[i] = new double[n];
 
-  for (int i = 0; i < n; i++)
-    delete mesh[i];
-  delete mesh;
+  // for (auto particle : this->particles) {
+  //   Vec3 v = particle->get_position();
+  //   double px = v.X();
+  //   double py = v.Y();
+  //   double pz = v.Z();
 
-  return lowest;
+  //   int ix = int(px/dL);
+  //   int iy = int(py/dL);
+
+  //   if ((ix > (n-1)) or (iy > (n-1)))
+  //     throw MathError("Index error in get_lowest_surface_height");
+
+  //   if (pz > mesh[ix][iy])
+  //     mesh[ix][iy] = pz;
+  // }
+
+  // double lowest = mesh[0][0];
+  // for (int i = 0; i < n; i++) {
+  //   for (int j = 0; j < n; j++) {
+  //     if (mesh[i][j] < lowest)
+  //       lowest = mesh[i][j];
+  //   }
+  // }
+
+  // for (int i = 0; i < n; i++)
+  //   delete mesh[i];
+  // delete mesh;
+
+  // return lowest;
 }
