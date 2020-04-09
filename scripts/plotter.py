1e10458950f3d9b451f4f1f597ac4e8f3edde23f
Chris Boyle <christopher.boyle.101@strath.ac.uk>
2020-04-09 11:27:10 +0100

    In plotter, warn when config ended in error

--- scripts/plotter.py
+++ scripts/plotter.py
@@ -15,7 +15,12 @@ def read_csv(path):
     with open(path) as csvf:
         lines = csvf.readlines()
 
-    L, vf, ff = (float(v) for v in lines[0].split(','))
+    header_data = [v for v in lines[0].split(',')]
+    L = float(header_data[0])
+    vf = float(header_data[1])
+    ff = float(header_data[2])
+    if header_data[-1].strip() == 'error':
+        print('Warning: sim ended in error.')
     particles = list()
     for line in lines[1:]:
         data = (float(v) for v in line.split(','))
