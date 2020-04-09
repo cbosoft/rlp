6d8b0de93d3d204aa6238a46b153c65b152ae7ec
Chris Boyle <christopher.boyle.101@strath.ac.uk>
2020-04-09 15:35:18 +0100

    Updated multi test

--- src/test/multi.hpp
+++ src/test/multi.hpp
@@ -12,25 +12,25 @@ class MultiInteractionTest : public virtual TestRunner<Vec3, Vec3> {
   public:
     
     MultiInteractionTest(bool is_quiet) 
-      : TestRunner("Line test (interaction)", is_quiet)
+      : TestRunner("Multi particle test", is_quiet)
     {
       this->input_data = {
-        Vec3({5.0, 5.1, 10.0})
+        Vec3({4.22479, 1.67952, 10.0})
       };
       this->expected_results = {
-        Vec3({5.0, 5.8660254037844383745, 0.0})
+        Vec3({4.0653970383976529845, 1.3030129329153037343, 0.77670979214002822122})
       };
     }
 
     void run(Vec3 point, Vec3 expected_result) override
     {
 
+      // build
       PeriodicBox box(10.0, this->is_quiet?-10:0);
       Particle *pi = new Particle(1.0, Vec3({4.71171, 0.118055, 10.0}));
       box.add_particle(pi);
       pi->set_position(Vec3({4.71171, 0.118055, 0.363677}));
 
-      try{
       Particle *pj = new Particle(1.0, Vec3({3.52381, 1.62458, 10.0}));
       box.add_particle(pj);
 
@@ -40,28 +40,19 @@ class MultiInteractionTest : public virtual TestRunner<Vec3, Vec3> {
       Particle *pl = new Particle(1.0, Vec3({4.05131, 0.775026, 10.0}));
       box.add_particle(pl);
 
-      Particle *pm = new Particle(1.0, Vec3({4.22479, 1.67952, 10.0}));
+      Particle *pm = new Particle(1.0, point);
+      box.add_particle(pm);
 
-      auto arrangements = box.get_arrangements();
-      for (auto a : arrangements) {
-        if (a->get_complexity() == 3)
-          std::cerr << a->repr() << " " << a->get_min_distance(pm) << " " << a->get_max_distance(pm) << std::endl;
+
+      Vec3 result = pm->get_position();
+      if (result == expected_result) {
+        this->pass();
+      }
+      else {
+        this->fail(Formatter() << "Particle did not settle to expected position. Expected " << expected_result << ", got " << result << ".");
       }
 
 
-        box.add_particle(pm);
-      }
-      catch (const Exception&) {
-      }
-
-      auto particles = box.get_particles();
-      std::cout << box.get_L() << std::endl;
-      for (auto p : particles) {
-        std::cout << p->get_diameter() << ", " << p->get_position().to_csv() << std::endl;
-      }
-
-      (void)point;
-      (void)expected_result;
     }
 
 };
