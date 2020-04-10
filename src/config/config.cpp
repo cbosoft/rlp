352f75e0aaecefa6f33f59f6d586be942164d16a
Chris Boyle <christopher.boyle.101@strath.ac.uk>
2020-04-10 23:17:51 +0100

    Output contact numbers with config

--- src/config/config.cpp
+++ src/config/config.cpp
@@ -31,7 +31,7 @@ void ConfigGenerator::output_configuration(bool failed)
   std::string exit_status = (failed ? "error" : "exited_normally");
   of << this->box.get_L() << ", " << this->box.get_volume_fraction() << ", " << this->box.get_friction_fraction() << ", " << exit_status << std::endl;
   for (const Particle *particle : this->box.get_particles()) {
-    of << particle->get_diameter() << ", " << particle->get_position().to_csv() << std::endl;
+    of << particle->get_diameter() << ", " << particle->get_position().to_csv() << ", " << particle->get_contact_number() << std::endl;
   }
 
 
