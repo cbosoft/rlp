4070985c002746b451be0f36ee46553219c68bb8
Chris Boyle <christopher.boyle.101@strath.ac.uk>
2020-04-10 23:18:12 +0100

    Read contact number from config

--- scripts/particle.py
+++ scripts/particle.py
@@ -3,7 +3,8 @@ import numpy as np
 
 class Particle:
 
-    def __init__(self, d, x, y, z, i):
+    def __init__(self, d, x, y, z, Z, i):
         self.diameter = d
         self.position = np.array([x, y, z], dtype=np.float64)
         self.i = i
+        self.Z = Z # contact number
