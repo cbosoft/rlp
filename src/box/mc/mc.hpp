e2b1d384e61db12a9068f640b279e8a8ce7c0b65
Chris Boyle <christopher.boyle.101@strath.ac.uk>
2020-04-12 12:17:41 +0100

    Started to flesh out MC box

--- src/box/mc/mc.hpp
+++ src/box/mc/mc.hpp
@@ -1,4 +1,12 @@
-#pragma
+#pragma once
+#include "../box.hpp"
 
-class MonteCarloBox {
+class MonteCarloBox : public virtual GenericBox {
+
+  public:
+
+    MonteCarloBox();
+    MonteCarloBox(double L, int verbosity=1, double friction_thresh=2.0) : GenericBox(L, verbosity, friction_thresh) {}
+
+    void add_particle(Particle *p) override;
 };
